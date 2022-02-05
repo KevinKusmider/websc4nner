7309474a71c50d37da7fa702e7adcd65636ed10e
Abdullah <abdullahhamednew@gmail.com>
2022-02-05 15:05:22 +0100

    GTK GLADE + CURL

--- src/interface.c
+++ src/interface.c
@@ -21,27 +21,27 @@ void activate (GtkApplication *app, gpointer user_data) {
   	gtk_window_set_title (GTK_WINDOW (window), "Window"); // Définir un nom a la fenetre
   	gtk_window_set_default_size (GTK_WINDOW (window), 1000, 800); // Définir la taille de la fenetre
 
-	boxBody = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-        gtk_container_add(GTK_CONTAINER (window), boxBody);
-        boxNav = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-        gtk_container_add(GTK_CONTAINER (boxBody), boxNav);
-        boxMain = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-        gtk_container_add(GTK_CONTAINER (boxBody), boxMain);
+	boxBody = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);// creer le Body
+        gtk_container_add(GTK_CONTAINER (window), boxBody);//ajouter le Body a la fenetre
+        boxNav = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);// creer la Nav 
+        gtk_container_add(GTK_CONTAINER (boxBody), boxNav);//ajouter la Nav au Body
+        boxMain = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);//creer le boxmain
+        gtk_container_add(GTK_CONTAINER (boxBody), boxMain);// ajouter la boxmain au Body
 
 
 	//box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 	// button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL); // Crétion d'une box en position horizontale
 	//gtk_container_add (GTK_CONTAINER (window), box); // AJout de la box
 
-	button = gtk_button_new_with_label ("Hello World");
-	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+	button = gtk_button_new_with_label ("Hello World"); //Ajouter du text au button
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);//lors du click active la print_hello
 	g_signal_connect_swapped (button, "clicked", G_CALLBACK (print_hello), window);
-	gtk_container_add (GTK_CONTAINER (boxNav), button);
+	gtk_container_add (GTK_CONTAINER (boxNav), button);//ajouter le button a la Nav
 
-	button2 = gtk_button_new_with_label ("Hello \n\n\n\nWorld");
-	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+	button2 = gtk_button_new_with_label ("Hello \n\n\n\nWorld");//Ajouter du text au button 2
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);//lors du click active la print_hello
 	g_signal_connect_swapped (button2, "clicked", G_CALLBACK (print_hello), window);
-	gtk_container_add (GTK_CONTAINER (boxNav), button2);
+	gtk_container_add (GTK_CONTAINER (boxNav), button2);//Ajouter le button 2 a la Nav
 
 	gtk_widget_show_all (window);
 }
